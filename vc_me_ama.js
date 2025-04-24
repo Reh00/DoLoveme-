f71f74751347f9fd949560cd359b4b7f7813a87d
Renan <renancarrinho@hotmail.com>
2025-04-24 17:09:26 -0300

    ajusted

--- vc_me_ama.js
+++ vc_me_ama.js
@@ -6,26 +6,19 @@ const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
 noBtn.addEventListener("mouseover", () => {
-  const offsetX = Math.floor(Math.random() * 100) - 50;
-  const offsetY = Math.floor(Math.random() * 100) - 50;
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
 
-  const currentLeft = noBtn.offsetLeft;
-  const currentTop = noBtn.offsetTop;
-
-  const newLeft = currentLeft + offsetX;
-  const newTop = currentTop + offsetY;
-
-  noBtn.style.position = "absolute";
-  noBtn.style.left = `${newLeft}px`;
-  noBtn.style.top = `${newTop}px`;
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
 });
 
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
-  heartLoader.style.display = "block";
-
-  setTimeout(() => {
+  heartLoader.style.display = "inherit";
+  const timeoutId = setTimeout(() => {
     heartLoader.style.display = "none";
-    resultContainer.style.display = "block";
+    resultContainer.style.display = "inherit";
+    gifResult.play();
   }, 3000);
 });
